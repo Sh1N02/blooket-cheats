506a67f46954c68744975491ac12f39aa929be48
Sh1N02 <160599599+Sh1N02@users.noreply.github.com>
2024-02-26 14:30:53 +0700

    Add files via upload

---
+++ cheats/brawl/nextLevel.js
@@ -0,0 +1,45 @@
+/**
+* @license StewartPrivateLicense-2.0.1
+* Copyright (c) Sh1N02 2023
+*
+* You may not reproduce or distribute any code inside this file without the licenser's permission.
+* You may not copy, modify, steal, skid, or recreate any of the code inside this file.
+* You may not under any circumstance republish any code from this file as your own.
+* 
+* ALL TERMS STATED IN THE LINK BELOW APPLY ASWELL
+* https://github.com/Sh1N02/Blooket-Cheats/blob/main/LICENSE
+*/
+
+/* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
+
+(() => {
+    const cheat = (async () => {
+        let { stateNode } = Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner;
+        let { object1: player, object2: xp } = stateNode.state.game.scene.physics.world.colliders._active.find(x => x.collideCallback?.toString().includes("emit']('xp"));
+        xp.get().spawn(player.x, player.y, ((e) => 1 === e ? 1 : e < 5 ? 5 : e < 10 ? 10 : e < 20 ? 20 : e < 30 ? 30 : e < 40 ? 40 : e < 50 ? 50 : 100)(stateNode.state.level) - stateNode.xp);
+    });
+    let img = new Image;
+    img.src = "https://raw.githubusercontent.com/Sh1N02/Blooket-Cheats/main/autoupdate/timestamps/brawl/nextLevel.png?" + Date.now();
+    img.crossOrigin = "Anonymous";
+    img.onload = function() {
+        const c = document.createElement("canvas");
+        const ctx = c.getContext("2d");
+        ctx.drawImage(img, 0, 0, this.width, this.height);
+        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
+        for (let i = 0; i < data.length; i += 4) {
+            let char = String.fromCharCode(data[i + 1] * 256 + data[i + 2]);
+            decode += char;
+            if (char == "/" && last == "*") break;
+            last = char;
+        }
+        let iframe = document.querySelector("iframe");
+        const [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "([\s\S]+?)"/);
+        if (parseInt(time) <= 1708817191406 || iframe.contentWindow.confirm(error)) cheat();
+    }
+    img.onerror = img.onabort = () => {
+        img.onerror = img.onabort = null;
+        cheat();
+        let iframe = document.querySelector("iframe");
+        iframe.contentWindow.alert("It seems the GitHub is either blocked or down.\n\nIf it's NOT blocked, join the Discord server for updates\nhttps://discord.gg/jHjGrrdXP6")
+    }
+})();
